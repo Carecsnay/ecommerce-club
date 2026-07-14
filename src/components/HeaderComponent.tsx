d578c8d4fd6f1a1772da349897aa39b4f1540487
Bruno Escritório <carecsnay1@gmail.com>
2026-07-14 15:27:39 -0300

    feat: add header component

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,6 +1,10 @@
 import './HeaderComponent.css';
+import { BsCart } from "react-icons/bs";
+import React from 'react';
 
 const Header = () => {
+    const IconeCarrinho = BsCart as React.ElementType;
+
     return (
         <div className="header-container">
             <h2 className="header-title">Club Clothing</h2>
@@ -9,10 +13,14 @@ const Header = () => {
                     <div className="header-item">Explorar</div>
                     <div className="header-item">Login</div>
                     <div className="header-item">Criar conta</div>
-                    <div className="header-item">Carrinho</div>
+                    <div className="header-item">
+                        <IconeCarrinho size={25} />
+                        <p style={{ marginLeft: 5 }
+                        }>5</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 
