27550a591a8caa241b5d5a427dc6d768e70c9217
Bruno Escritório <carecsnay1@gmail.com>
2026-07-15 14:52:19 -0300

    refactor: rename components project

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import Category from '../../types/category.type';
 import './categories.style.css'
 
 const Categories = () => {
-    return (<></>);
+    const [categories, setCategories] = useState<Category[]>([])
+
+
+    return (
+        <div className="categories-container">
+            <div className="categories-content">
+
+            </div>
+        </div>
+    );
 }
 
 export default Categories;
