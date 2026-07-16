bbc078ed4ae6be27e1322e7508825fa2f5273b51
Bruno Escritório <carecsnay1@gmail.com>
2026-07-16 16:04:35 -0300

    feat: implements fetch on component

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Category from '../../types/category.type';
 import './categories.style.css'
+import axios from 'axios';
+import env from '../../config/env.config';
 
 const Categories = () => {
     const [categories, setCategories] = useState<Category[]>([])
 
+
+    const fetchCategories = async () => {
+        try {
+            const { data } = await axios.get(`${env.apiUrl}/category`)
+
+            setCategories(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    console.log(categories)
+
+    useEffect(() => { fetchCategories() }, [])
 
     return (
         <div className="categories-container">
