cdd55ffa6a86d3124a55d90b664470c23a739896
Bruno Asus <carecsnay1@gmail.com>
2026-07-16 20:18:57 -0300

    refactor: update imports

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,5 +1,5 @@
 import Categories from "../../components/categories/categories.component";
-import Header from "../../components/header.component"
+import Header from "../../components/header/header.component"
 
 const HomePage = () => {
     return (
