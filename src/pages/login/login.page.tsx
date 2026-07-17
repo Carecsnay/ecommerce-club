2ece5a064d4902e13d0577e64ee3b4f9ba1d6987
Bruno Escritório <carecsnay1@gmail.com>
2026-07-17 15:08:44 -0300

    feat: add new custom button "Entrar"

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,8 +1,11 @@
 import { BsGoogle } from "react-icons/bs";
+import { CiLogin } from "react-icons/ci";
+
 import CustomButton from "../../components/custom-button/custom-button.component";
 import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from "./login.style";
 
 const BSGoogle = BsGoogle as React.ElementType;
+const FILogin = CiLogin as React.ElementType;
 
 const LoginPage = () => {
     return (
@@ -10,11 +13,11 @@ const LoginPage = () => {
             <LoginContainer>
                 <LoginContent>
                     <LoginHeadline>Entre com a sua conta</LoginHeadline>
-                    <CustomButton name="Entrar com o google" icon={<BSGoogle size={25} />} />
+                    <CustomButton name="Entrar com a conta google" icon={<BSGoogle size={22} />} />
                     <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
                     <LoginInputContainer>{/* email input */}</LoginInputContainer>
                     <LoginInputContainer>{/* password input */}</LoginInputContainer>
-                    {/* button */}
+                    <CustomButton name="Entrar" icon={<FILogin size={25} />} />
                 </LoginContent>
             </LoginContainer>
         </div>
