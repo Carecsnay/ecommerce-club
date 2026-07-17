46465088bdbfceeae97573a72591382a5f8fbf21
Bruno Escritório <carecsnay1@gmail.com>
2026-07-17 10:44:10 -0300

    fix: header behavior

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/home/home.page";
 import LoginPage from "./pages/login/login.page";
+import Header from "./components/header/header.component";
 
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,13 +1,11 @@
 import Categories from "../../components/categories/categories.component";
-import Header from "../../components/header/header.component"
 
 const HomePage = () => {
     return (
         <div>
-            <Header />
             <Categories />
         </div>
     );
-}
+};
 
 export default HomePage;
