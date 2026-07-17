26336a0574c4d6eb6277cda505714e3a239f0fb1
Bruno Escritório <carecsnay1@gmail.com>
2026-07-17 14:45:15 -0300

    feat: create login page

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,5 +1,24 @@
+import { BsGoogle } from "react-icons/bs";
+import CustomButton from "../../components/custom-button/custom-button.component";
+import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from "./login.style";
+
+const IconeCarrinho = BsGoogle as React.ElementType;
+
 const LoginPage = () => {
-    return <>A vida é um conto de falhas!</>;
+    return (
+        <div>
+            <LoginContainer>
+                <LoginContent>
+                    <LoginHeadline>Entre com a sua conta</LoginHeadline>
+                    <CustomButton name="Entrar com o google" icon={<IconeCarrinho size={25} />} />
+                    <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
+                    <LoginInputContainer>{/* email input */}</LoginInputContainer>
+                    <LoginInputContainer>{/* password input */}</LoginInputContainer>
+                    {/* button */}
+                </LoginContent>
+            </LoginContainer>
+        </div>
+    );
 };
 
 export default LoginPage;
