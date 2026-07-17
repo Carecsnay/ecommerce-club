1aa6a2cb0be74677f60ae58e46c4bdb47c41603f
Bruno Escritório <carecsnay1@gmail.com>
2026-07-17 15:35:52 -0300

    feat: use custom input component

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -3,6 +3,7 @@ import { CiLogin } from "react-icons/ci";
 
 import CustomButton from "../../components/custom-button/custom-button.component";
 import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from "./login.style";
+import CustomInput from "../../components/custom-input/custom-input.component";
 
 const BSGoogle = BsGoogle as React.ElementType;
 const FILogin = CiLogin as React.ElementType;
@@ -15,8 +16,12 @@ const LoginPage = () => {
                     <LoginHeadline>Entre com a sua conta</LoginHeadline>
                     <CustomButton name="Entrar com a conta google" icon={<BSGoogle size={22} />} />
                     <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
-                    <LoginInputContainer>{/* email input */}</LoginInputContainer>
-                    <LoginInputContainer>{/* password input */}</LoginInputContainer>
+                    <LoginInputContainer>
+                        <CustomInput placeholder="Digite seu e-mail" />
+                    </LoginInputContainer>
+                    <LoginInputContainer>
+                        <CustomInput placeholder="Digite sua senha" />
+                    </LoginInputContainer>
                     <CustomButton name="Entrar" icon={<FILogin size={25} />} />
                 </LoginContent>
             </LoginContainer>
