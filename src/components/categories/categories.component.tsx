3cb3513e01b7e4a7e46cc5eca32b30e3881063d6
Bruno Asus <carecsnay1@gmail.com>
2026-07-16 21:19:01 -0300

    feat: show categories itens om home page

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,34 +1,42 @@
-import { useEffect, useState } from 'react';
-import Category from '../../types/category.type';
-import './categories.style.css'
-import axios from 'axios';
-import env from '../../config/env.config';
+import Category from "../../types/category.type";
+import CategoryItem from "../category-item/categories-item.component";
+import axios from "axios";
+import env from "../../config/env.config";
+
+import { useEffect, useState } from "react";
+
+import "./categories.style.css";
 
 const Categories = () => {
-    const [categories, setCategories] = useState<Category[]>([])
-
+    const [categories, setCategories] = useState<Category[]>([]);
 
     const fetchCategories = async () => {
         try {
-            const { data } = await axios.get(`${env.apiUrl}/category`)
+            const { data } = await axios.get(`${env.apiUrl}/category`);
 
-            setCategories(data)
+            setCategories(data);
         } catch (error) {
-            console.error(error)
+            console.log(error);
         }
-    }
+    };
 
-    console.log(categories)
+    console.log(categories);
 
-    useEffect(() => { fetchCategories() }, [])
+    useEffect(() => {
+        fetchCategories();
+    }, []);
 
     return (
         <div className="categories-container">
             <div className="categories-content">
-
+                {categories.map((category) => (
+                    <div key={category.id}>
+                        <CategoryItem category={category} />
+                    </div>
+                ))}
             </div>
         </div>
     );
-}
+};
 
 export default Categories;
