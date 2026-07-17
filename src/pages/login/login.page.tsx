376c315b02591e7e3f3b08febacce847c6eed51a
Bruno Escritório <carecsnay1@gmail.com>
2026-07-17 14:54:56 -0300

    refactor: change name icon

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -2,7 +2,7 @@ import { BsGoogle } from "react-icons/bs";
 import CustomButton from "../../components/custom-button/custom-button.component";
 import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from "./login.style";
 
-const IconeCarrinho = BsGoogle as React.ElementType;
+const BSGoogle = BsGoogle as React.ElementType;
 
 const LoginPage = () => {
     return (
@@ -10,7 +10,7 @@ const LoginPage = () => {
             <LoginContainer>
                 <LoginContent>
                     <LoginHeadline>Entre com a sua conta</LoginHeadline>
-                    <CustomButton name="Entrar com o google" icon={<IconeCarrinho size={25} />} />
+                    <CustomButton name="Entrar com o google" icon={<BSGoogle size={25} />} />
                     <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
                     <LoginInputContainer>{/* email input */}</LoginInputContainer>
                     <LoginInputContainer>{/* password input */}</LoginInputContainer>
