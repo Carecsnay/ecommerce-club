e4f7e1b89a196b8a11687538756bb4a93a6d55ad
Bruno Asus <carecsnay1@gmail.com>
2026-07-17 06:35:52 -0300

    feat: define color palette with styled components

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+    primary: "#212539",
+    text: {
+        white: "#f8f9fa",
+        black: "#363a40",
+    },
+    background: {
+        white: "#f8f9fa",
+        dark: "#212529",
+    },
+    input: {
+        background: "#e9ecef",
+        placeholder: "#6c757d",
+    },
+    error: "#ff6a6a",
+};
+
+export default Colors;
