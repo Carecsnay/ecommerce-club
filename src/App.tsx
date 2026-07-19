1fa86946892b97033f5c5bbf2695a042def68759
Bruno Asus <carecsnay1@gmail.com>
2026-07-19 09:08:39 -0300

    feat: add SignUpPage router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+
+import Header from "./components/header/header.component";
+
 import HomePage from "./pages/home/home.page";
 import LoginPage from "./pages/login/login.page";
-import Header from "./components/header/header.component";
+import SignUpPage from "./pages/sign-up/sign-up.page";
 
 function App() {
     return (
@@ -11,6 +14,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/sign-up" element={<SignUpPage />} />
             </Routes>
         </BrowserRouter>
     );
