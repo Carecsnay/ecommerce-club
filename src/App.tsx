9bb3c88728e018ba203caa5bf02b547c9cd0a8c7
Bruno Escritório <carecsnay1@gmail.com>
2026-07-22 10:26:04 -0300

    feat: add listern to auth state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { onAuthStateChanged } from "firebase/auth";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 
 import Header from "./components/header/header.component";
-
 import HomePage from "./pages/home/home.page";
 import LoginPage from "./pages/login/login.page";
 import SignUpPage from "./pages/sign-up/sign-up.page";
 
+import "./App.css";
+
+import { auth } from "./config/firebase.config";
+
 function App() {
+    onAuthStateChanged(auth, (user) => {
+        console.log(user);
+    });
     return (
         <BrowserRouter>
             <Header />
