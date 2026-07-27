5c7fac12503ca0a48a1a26d4e85da6f20f77e717
Bruno Asus <carecsnay1@gmail.com>
2026-07-27 20:55:13 -0300

    feat: implement add to cart logic using CartContext

--- src/components/product-item/product-item.component.tsx
+++ src/components/product-item/product-item.component.tsx
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { BsCartPlus } from "react-icons/bs";
+import { CartContext } from "../../context/cart.context";
 import Product from "../../types/products.type";
+import CustomButton from "../custom-button/custom-button.component";
 import { ProductContainer, ProductImage, ProductInfo } from "./product.item.style";
 
 interface ProductItemProps {
@@ -6,10 +10,20 @@ interface ProductItemProps {
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
+    const { addProductToCart } = useContext(CartContext);
+    const handleAddProductToCart = () => {
+        addProductToCart(product);
+    };
     return (
         <>
             <ProductContainer>
-                <ProductImage $imageUrl={product.imageUrl}></ProductImage>
+                <ProductImage $imageUrl={product.imageUrl}>
+                    <CustomButton
+                        onClick={handleAddProductToCart}
+                        name={"Adicionar ao Carrinho"}
+                        icon={<BsCartPlus size={16} />}
+                    />
+                </ProductImage>
                 <ProductInfo>
                     <p>{product.name}</p>
                     <p>R$ {product.price}</p>
--- src/context/cart.context.tsx
+++ src/context/cart.context.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState } from "react";
 import CartProduct from "../types/cart.type";
+import Product from "../types/products.type";
 
 interface CartContextProviderProps {
     children: ReactNode;
@@ -9,20 +10,32 @@ interface ICartContext {
     isVisible: boolean;
     products: CartProduct[];
     toggleCart: () => void;
+    addProductToCart: (product: Product) => void;
 }
 
-export const CartContext = createContext<ICartContext>({ isVisible: false, products: [], toggleCart: () => {} });
+export const CartContext = createContext<ICartContext>({
+    isVisible: false,
+    products: [],
+    toggleCart: () => {},
+    addProductToCart: () => {},
+});
 
 const CartContextProvider = ({ children }: CartContextProviderProps) => {
     const [isVisible, setVisible] = useState(false);
     const [products, setProducts] = useState<CartProduct[]>([]);
+
+    const addProductToCart = (product: Product) => {
+        setProducts((prevStage) => [...prevStage, { ...product, quantity: 1 }]);
+    };
 
     const toggleCart = () => {
         setVisible((prevState) => !prevState);
     };
     return (
         <>
-            <CartContext.Provider value={{ isVisible, products, toggleCart }}>{children}</CartContext.Provider>
+            <CartContext.Provider value={{ isVisible, products, toggleCart, addProductToCart }}>
+                {children}
+            </CartContext.Provider>
         </>
     );
 };
