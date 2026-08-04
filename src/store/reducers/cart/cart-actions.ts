ef76dae33165d49a32a972997eae33ba6c109e54
Bruno Escritório <carecsnay1@gmail.com>
2026-08-04 09:27:18 -0300

    feat: add cart reducer

--- src/store/reducers/cart/cart-actions.ts
+++ src/store/reducers/cart/cart-actions.ts
@@ -1,5 +1,13 @@
+import Product from "../../../types/products.type";
+import CartActionTypes from "./cart-actions.types";
+
 export const toggleCart = () => ({
-    type: "cart/toggle",
+    type: CartActionTypes.toggleCart,
+});
+
+export const addProductToCart = (payload: Product) => ({
+    type: CartActionTypes.addProductToCart,
+    payload,
 });
 
 export default toggleCart;
--- src/store/reducers/cart/cart-reducer.ts
+++ src/store/reducers/cart/cart-reducer.ts
@@ -19,6 +19,26 @@ const cartReducer = (state = initialState, action: any) => {
     switch (action.type) {
         case CartActionTypes.toggleCart:
             return { ...state, isVisible: !state.isVisible };
+        case CartActionTypes.addProductToCart: {
+            const product = action.payload;
+            const products = state.products;
+
+            //Verificar se o produto já está no carrinho por meio do ID
+            const productItsExists = products.some((item) => item.id === product.id);
+
+            //Se o produto existir aumentamos somente sua quantidade
+            if (productItsExists) {
+                return {
+                    ...state,
+                    products: state.products.map((item) =>
+                        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item,
+                    ),
+                };
+            }
+
+            //Se não adiciona o novo item a lista
+            return { ...state, products: [...state.products, { ...product, quantity: 1 }] };
+        }
         default:
             return { ...state };
     }
