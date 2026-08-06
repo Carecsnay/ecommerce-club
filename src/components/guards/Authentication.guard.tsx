59b298a29944ff5a6786d2e3fefdc3ac3c210696
Bruno Asus <carecsnay1@gmail.com>
2026-08-06 20:42:05 -0300

    refactor: remove unusable libs

--- src/components/guards/Authentication.guard.tsx
+++ src/components/guards/Authentication.guard.tsx
@@ -1,5 +1,4 @@
 import { ReactNode, useEffect } from "react";
-import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import LoadingComponent from "../loading/loading.component";
