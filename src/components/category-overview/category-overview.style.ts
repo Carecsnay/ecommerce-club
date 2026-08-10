d93f8e289f68ab412114b6556054d6ffbec2a496
Bruno Escritório <carecsnay1@gmail.com>
2026-08-10 16:12:13 -0300

    style: fix the categories layout

--- src/components/category-overview/category-overview.style.ts
+++ src/components/category-overview/category-overview.style.ts
@@ -3,33 +3,34 @@ import Colors from "../../theme/theme.colors";
 
 export const CategoryContainer = styled.div`
     display: flex;
-    align-items: center;
-    justify-content: center;
     flex-direction: column;
     width: 100%;
     margin-top: 15px;
+
+    @media (max-width: 768px) {
+        max-width: 300px;
+    }
 `;
 
 export const CategoryTitle = styled.p`
     background-color: ${Colors.background.dark};
     color: ${Colors.text.white};
     border-radius: 10px;
+    padding: 5px 10px 5px 10px;
     margin-bottom: 15px;
-    padding: 10px 15px;
     text-align: start;
-    font-size: 1.25rem;
+    font-size: 21px;
     font-weight: 500;
+    padding: 10px 15px;
 
     @media (max-width: 768px) {
-        font-size: 1.25rem;
-        width: 97.5%;
         text-align: center;
-    }
+}
 `;
 
 export const ProductsContainer = styled.div`
     display: flex;
     width: 100%;
-    justify-content: center;
+    justify-content: space-between;
     flex-wrap: wrap;
 `;
