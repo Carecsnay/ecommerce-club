f2238704ea30baa388627000e36cc614c62c7991
Bruno Escritório <carecsnay1@gmail.com>
2026-08-14 16:24:59 -0300

    test: unsure show correct cart products QUANTITY

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,4 +1,5 @@
 import { renderWithRedux } from "../../helpers/text.helpers";
+import CartProduct from "../../types/cart.type";
 import Header from "./header.component";
 
 describe("Header", () => {
@@ -17,5 +18,22 @@ describe("Header", () => {
 
         getByText(/criar conta/i);
         getByText(/login/i);
+    });
+
+    it("should show correct cart products QUANTITY", () => {
+        const products: CartProduct[] = [
+            { id: "1", imageUrl: "image-url", quantity: 1, name: "calça", price: 100 },
+            { id: "2", imageUrl: "image-url", quantity: 2, name: "jaqueta", price: 200 },
+        ];
+
+        const { getByText } = renderWithRedux(<Header />, {
+            preloadedState: {
+                cartReducer: {
+                    products: products,
+                },
+            } as any,
+        });
+
+        getByText("3");
     });
 });
