9b0f25867fcbe8daccf4aef0af0935a4dda4b918
Bruno Escritório <carecsnay1@gmail.com>
2026-08-24 10:41:07 -0300

    test: ensure shows error when email input is a invalid value

--- src/pages/login/login.test.tsx
+++ src/pages/login/login.test.tsx
@@ -1,7 +1,6 @@
 import userEvent from "@testing-library/user-event";
 import { renderWithRedux } from "../../helpers/text.helpers";
 import LoginPage from "./login.page";
-import { getByText } from "@testing-library/dom";
 
 describe("Login", () => {
     it("Should show error when trying to submit witout filling required fields ", async () => {
@@ -12,5 +11,17 @@ describe("Login", () => {
 
         await findByText("O email é obrigatório.");
         getByText("A senha é obrigatória.");
+    });
+
+    it("Should show error when user type a invalid email", async () => {
+        const { getByPlaceholderText, findByText, getByText } = renderWithRedux(<LoginPage />, {});
+
+        const emailInput = getByPlaceholderText("Digite seu e-mail");
+        userEvent.type(emailInput, "INVALID_EMAIL");
+
+        const submitButton = getByText("Entrar");
+        userEvent.click(submitButton);
+
+        await findByText("Digite um e-mail válido (ex: nome@email.com).");
     });
 });
